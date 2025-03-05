9d10fcee414721705374c6eff4b1e6e853d19523
cheroliv <cheroliv@laposte.net>
2025-03-05 02:02:03 +0100

    Update .gitignore and edster.jdl to include new entries and entities

--- jdl.sh
+++ jdl.sh
@@ -24,44 +24,56 @@ else
   echo "No tables found in schema '$SCHEMA_NAME' owned by '$DB_USER'."
 fi
 
-echo "Tables dans le schéma '$SCHEMA_NAME' supprimées."
+echo "Tables in schema '$SCHEMA_NAME' been dropped."
 
 rm -R .devcontainer;
 rm -R .goose;
 rm -R .gradle;
 rm -R .husky;
 rm -R .jhipster;
-rm -R build;
+rm -R build/reports;
+rm -R build/tmp;
 rm -R buildSrc;
 rm -R gradle;
 rm -R src;
 rm -R webpack;
+rm -R build/classes
+rm -R build/generated
+rm -R build/jacoco
+rm -R build/openapi
+rm -R build/resources
+rm -R build/test-results
+rm -R build/webpack
 
-rm .editorconfig
-rm .gitattributes
+rm build/resolvedMainClassName
+rm .editorconfig;
+rm .gitattributes;
 #rm .gitignore
-rm .lintstagedrc.cjs
-rm .prettierignore
-rm .prettierrc
-rm .yo-rc.json
-rm build.gradle
-rm checkstyle.xml
-rm cypress.config.ts
-rm cypress-audits.config.ts
-rm eslint.config.mjs
-rm gradle.properties
-rm gradlew
-rm gradlew.bat
-rm jest.conf.js
-rm npmw
-rm npmw.cmd
-rm package.json
-rm package-lock.json
-rm postcss.config.js
-rm README.md
-rm settings.gradle
-rm sonar-project.properties
-rm tsconfig.json
-rm tsconfig.test.json
-#jhipster jdl edster.jdl --force
-#echo ".goose" >> .gitignore
+rm .lintstagedrc.cjs;
+rm .prettierignore;
+rm .prettierrc;
+rm .yo-rc.json;
+rm build.gradle;
+rm checkstyle.xml;
+rm cypress.config.ts;
+rm cypress-audits.config.ts;
+rm eslint.config.mjs;
+rm gradle.properties;
+rm gradlew;
+rm gradlew.bat;
+rm jest.conf.js;
+rm pnpmw;
+rm pnpmw.cmd;
+rm npmw;
+rm npmw.cmd;
+rm package.json;
+rm package-lock.json;
+rm pnpm-lock.yaml;
+rm postcss.config.js;
+rm README.md;
+rm settings.gradle;
+rm sonar-project.properties;
+rm tsconfig.json;
+rm tsconfig.test.json;
+jhipster jdl edster.jdl --force;
+echo ".goose" >> .gitignore;
