e519356fb34a02e9c9fea90f0194eedbced71b91
cheroliv <cheroliv@laposte.net>
2025-03-08 23:09:30 +0100

    update footer.tsx

--- __codebase__/src/main/webapp/app/shared/layout/footer/footer.tsx
+++ __codebase__/src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -11,13 +11,28 @@ const Footer = () => {
           <div className="col-md-6 text-md-right">
             <ul className="list-inline">
               <li className="list-inline-item">
-                <a style={{ color: 'white' }} href="https://github.com/cccp-education" target="_blank" rel="noopener noreferrer">
+                <a style={{ color: 'white' }} href="https://cheroliv.github.io/blog.html" target="_blank" rel="noopener noreferrer">
+                  Blog
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a style={{ color: 'white' }} href="https://cheroliv.github.io/about.html" target="_blank" rel="noopener noreferrer">
+                  Documentation
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a style={{ color: 'white' }} href="https://github.com/cccp-education/" target="_blank" rel="noopener noreferrer">
                   GitHub
                 </a>
               </li>
               <li className="list-inline-item">
-                <a style={{ color: 'white' }} href="https://youtube.com/@cccp.education" target="_blank" rel="noopener noreferrer">
+                <a style={{ color: 'white' }} href="https://youtube.com/@cccp.education/" target="_blank" rel="noopener noreferrer">
                   YouTube
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a style={{ color: 'white' }} href="https://cheroliv.github.io/about.html" target="_blank" rel="noopener noreferrer">
+                  About
                 </a>
               </li>
             </ul>
